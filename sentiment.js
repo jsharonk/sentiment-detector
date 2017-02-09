0d2b9306ab4aa473ca8fae80f4fc6a32d6cb0611
jsk <jsharonkim@gmail.com>
2017-02-09 15:29:05 -0500

    initial commit

--- sentiment.js
+++ sentiment.js
@@ -1,92 +1,77 @@
 var fs = require("fs");
 
 function getRecordsFromFile(filename, cb) {
-  fs.readFile(filename, "utf8", function(err, data) {
-    if (err)  {
+  fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) {
       console.log("---Error---");
       console.log(err);
       return;
     }
-    var records = data.split("\n"); //Is this split on newlines to make it an array?
-    // console.log(records);
+
+    var records = data.split("\n");
+
     cb(records);
-  });
+  })
 }
 
-
-
-function recordWord(record) {
-  var recordWord = (record.split(" ")[2]).split("=")[1];
-
-  return recordWord.toLowerCase();
+function parseWord(record) {
+  var wordRecord = record.split(" ")[2];
+  return wordRecord.split("=")[1];
 }
 
-
-function wordRating(record) {
-  return ((record.split(" ")[5]).split("=")[1]);
-  // var rating = (record.split(" ")[5]).split("=")[1];
-  // if (rating === "positive") {
-  //   return true;
-  // }
-  // return false;
+function parseSentiment(record) {
+  var sentimentRecord = record.split(" ")[5];
+  return sentimentRecord.split("=")[1];
 }
-
-
-
 
 function detectSentiment(filename, sentence, cb) {
-
   getRecordsFromFile(filename, function(records) {
-
-    var sentenceWords = sentence.toLowerCase().split(" ");
     var score = 0;
+    var splitSentence = sentence.toLowerCase().split(" ");
 
     records.forEach(function(record) {
-      // if (!record) {       //what is this part about
-      //   return;
-      // }
+      if (!record) {
+        return;
+      }
 
-      var word = recordWord(record);
-      var rating = wordRating(record);
+      var word = parseWord(record).toLowerCase();
+      var sentiment = parseSentiment(record);
 
-      if (sentenceWords.indexOf(word) !== -1) {
-        if (rating === "positive") {
-            score += 1;
-        } else if (rating === "negative") {
-            score -=1;
+      if (splitSentence.indexOf(word) !== -1) {
+        if (sentiment === "negative") {
+          score -= 1;
+        } else if (sentiment === "positive") {
+          score += 1;
         }
       }
     });
 
-
-      cb(sentence, score);
+    cb(sentence, score);
   });
-
 }
 
-
 detectSentiment("sentimentDict.txt", "I love you", function(sentence, score) {
-  console.log("~~~~Example 1~~~~");
-  console.log(" ' " + sentence + " ' "  + " has a score of " + score + ".");
+  console.log("~~~~Example 1~~~~")
+  console.log("'" + sentence + "' has a score of " + score + ".");
 });
 
 detectSentiment("sentimentDict.txt", "I LOVE you so much", function(sentence, score) {
-  console.log("~~~~Example 2~~~~");
-  console.log(" ' " + sentence + " ' "  + " has a score of " + score + ".");
+  console.log("~~~~Example 2~~~~")
+  console.log("'" + sentence + "' has a score of " + score + ".");
 });
 
-detectSentiment("sentimentDict.txt", "You are a loveless fool", function(sentence, score) {
-  console.log("~~~~Example 3~~~~");
-  console.log(" ' " + sentence + " ' "  + " has a score of " + score + ".");
+detectSentiment("sentimentDict.txt", "You are a loveless parrot, unfortunately", function(sentence, score) {
+  console.log("~~~~Example 3~~~~")
+  console.log("'" + sentence + "' has a score of " + score + ".");
 });
 
-detectSentiment("sentimentDict.txt", "I cherish your smile", function(sentence, score) {
-  console.log("~~~~Example 4~~~~");
-  console.log(" ' " + sentence + " ' "  + " has a score of " + score + ".");
+detectSentiment("sentimentDict.txt", "I cherish the pomegranate", function(sentence, score) {
+  console.log("~~~~Example 4~~~~")
+  console.log("'" + sentence + "' has a score of " + score + ".");
 });
 
-var sentence = "I despise your hateful attitude";
+var sentence = "I despise the softness of those pickles;
 detectSentiment("sentimentDict.txt", sentence, function(sentence, score) {
-  console.log("~~~~Example 5~~~~");
-  console.log(" ' " + sentence + " ' "  + " has a score of " + score + ".");
+  console.log("~~~~Example 5~~~~")
+  console.log("'" + sentence + "' has a score of " + score + ".");
 });
